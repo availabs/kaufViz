3b4ccaf6f3acdb4c4344ec8e132fedc1c7381868
cakesofwrath <dai.alan.dai@gmail.com>
2015-11-10 16:45:17 -0500

    es6-ified MetroZBP

--- assets/react/pages/MetroZBP_es6.js
+++ assets/react/pages/MetroZBP_es6.js
@@ -110,7 +110,7 @@ var Header = React.createClass({
             };
 
         let getDetails = d3.promise.json(api + "/details").post(JSON.stringify({
-            fips, 
+            fips,
             "year": this.state.year
         }));
         getDetails.then((res) => {
@@ -151,10 +151,329 @@ var Header = React.createClass({
                     loading: false
                 });
             })
-            
+
         })
     },
-    
+
+    getLocalData() {
+        // unneeded for now
+    },
+
+    componentDidMount() {
+        this.getMetroData(this.state.metroFips);
+        buble.drawLegends();
+    },
+
+    getGeography(fips, zips, cb) {
+        let api = "http://zbp.availabs.org";
+        let getZips = d3.promise.json(api + "/geozipcodes").post(JSON.stringify({
+            zips
+        }));
+        let getFips = d3.promise.json(api + "/geozipcodes").post(JSON.stringify({
+            fips
+        }));
+
+        getZips.then((zipsData) => {
+            getFips.then((fipsData) => {
+                fipsData.features[0].properties.type="metro"
+                zipsData.features = zipsData.features.concat(fipsData.features)
+
+                cb(zipsData)
+            });
+        });
+    },
+
+    getCircleArray(data) {
+        let scope = this,
+            circleArray = [];
+
+        circleArray = Object.keys(data).map((zipKey) => {
+            return Object.keys(data[zipKey]).map((naicsKey) => {
+                return Object.keys(data[zipKey][naicsKey]).map((sizeKey) => {
+                    let cluster = naicsKey.substr(0, 2);
+                    if(cluster !== "--" && naicsLib[cluster].part_of_range) {
+                        cluster = naicsLib[cluster].part_of_range;
+                    }
+
+                    return {
+                        cluster,
+                        naics: naicsKey,
+                        size: sizeKey.split("-")[0],
+                        radius: sizeKey.split("-")[0],
+                        count: +data[zipKey][naicsKey][sizeKey],
+                        zip: zipKey
+                    };
+                });
+            });
+        });
+
+        let flat1 = [],
+            flat2 = [],
+            flat3 = [];
+
+        flat1 = flat1.concat.apply(flat1, circleArray);
+        flat2 = flat2.concat.apply(flat2, flat1);
+
+        circleArray = circleArray.map((d) => {
+            let output = []
+            for(let i = 0;i < d.count;i++){
+                output.push(d);
+            }
+            return output;
+        });
+
+        flat3 = flat3.concat.apply(flat2, circleArray);
+
+        circleArray = flat3.filter((d) => {
+            return d.radius !== "total" && d.count > 0 && d.naics.substr(0,2) !== "--";
+        });
+
+        circleArray = circleArray.map((d) => {
+            if(d.radius === "1000+"){
+                d.radius = 1000;
+            }
+            return d;
+        });
+        //console.log("num establishments:",circleArray.length)
+        return circleArray;
+    },
+
+    componentDidUpdate(nextProps, nextState) {
+        bubble.renderBubbleChart(this.state.data,this.props.mapWidth,this.props.mapHeight,map.centroids,this.state.options);
+    },
+
+    renderIndustryAnalysis() {
+        let scope = this,
+            estCount = 0,
+            estEmp = 0,
+            indCount = 0,
+            indEmp = 0,
+            indCountPer = 0,
+            indEmpPer = 0;
+
+
+        if(this.state.options.naics.depth === 0){
+            return <span />
+        }
+        else if(this.state.options.naics.depth === 1){
+
+            let estCount = this.state.data.length,
+                estEmp = this.state.data.reduce((a,b) => { return parseInt(a) + parseInt(b.radius) },0);
+
+            let filterData = scope.state.data.filter((d){
+                return d.cluster === scope.state.options.naics.code;
+            })
+
+            indCount = filterData.length;
+            indEmp = filterData.reduce((a,b) => { return parseInt(a) + parseInt(b.radius) },0);
+            indCountPer = Math.round((indCount / estCount)*100);
+            indEmpPer = Math.round((indEmp / estEmp)*100);
+
+        }
+        else if(this.state.options.naics.depth > 1){
+
+            estCount = this.state.data.length;
+            estEmp = this.state.data.reduce((a,b) => { return parseInt(a) + parseInt(b.radius) },0);
+
+            let filterData = scope.state.data.filter((d) => {
+                return d.naics.substr(0,scope.state.options.naics.code.length) === scope.state.options.naics.code;
+            });
+
+            indCount = filterData.length;
+            indEmp = filterData.reduce((a,b) => { return parseInt(a) + parseInt(b.radius) },0);
+            indCountPer = Math.round((indCount / estCount)*100);
+            indEmpPer = Math.round((indEmp / estEmp)*100);
+
+        }
+
+        return (
+                <div>
+                    <div className="row">
+                        <div className="col-xs-4" style={{textAlign:"center",padding:6,fontSize:14}}>
+                            <strong>Establishments</strong>
+                        </div>
+
+                        <div className="col-xs-4" style={{textAlign:"center",padding:6,fontSize:14}}>
+                            {nf.format(indCount)}
+                        </div>
+                        <div className="col-xs-4" style={{textAlign:"center",padding:6,fontSize:14}}>
+                            {nf.format(indCountPer)}%
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-xs-4" style={{textAlign:"center",padding:6,fontSize:14}}>
+                            <strong>Employment</strong>
+                        </div>
+                        <div className="col-xs-4" style={{textAlign:"center",padding:6,fontSize:14}}>
+                            {nf.format(indEmp)}
+                        </div>
+                        <div className="col-xs-4" style={{textAlign:"center",padding:6,fontSize:14}}>
+                            {nf.format(indEmpPer)}%
+                        </div>
+                    </div>
+                </div>
+
+        )
+    },
+
+    renderControls() {
+
+        let zipcodeCount = "",
+            estCount = "",
+            estEmp = "",
+            cluster =  this.state.options.mode === "cluster" ? " active" : "",
+            zips =  this.state.options.mode === "zips" ? " active" : "";
+
+        if(this.state.data.length > 0){
+            zipcodeCount = this.state.geo.features.length -1,
+            estCount = this.state.data.length,
+            estEmp = this.state.data.reduce((a,b) => { return parseInt(a) + parseInt(b.radius) },0)
+            console.log("empEst", estEmp)
+        }
+
+
+        return (
+            <div className="col-md-12">
+                <div className="row">
+                    <div className="col-xs-12" style={{textAlign:"center",padding:6,fontSize:16}}>
+                        <strong>
+
+                            <SelectBox
+                                name="metroarea"
+                                value={this.state.metroFips}
+                                options={metros}
+                                onChange={this.updateMetro}/>
+
+                        </strong>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-4" style={{textAlign:"center",padding:6,fontSize:14}}>
+                        <strong>Year</strong>
+                    </div>
+                    <div className="col-xs-8" style={{textAlign:"center",padding:6,fontSize:14}}>
+                        <SelectBox
+                            name="datayear"
+                            value={this.state.year}
+                            options={years}
+                            onChange={this.updateYear}/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-4" style={{textAlign:"center",padding:6,fontSize:14}}>
+                        <strong># Zipcodes</strong>
+                    </div>
+                    <div className="col-xs-8" style={{textAlign:"center",padding:6,fontSize:14}}>
+                        {nf.format(zipcodeCount)}
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-4" style={{textAlign:"center",padding:6,fontSize:14}}>
+                        <strong>Establishments</strong>
+                    </div>
+                    <div className="col-xs-8" style={{textAlign:"center",padding:6,fontSize:14}}>
+                        {nf.format(estCount)}
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-4" style={{textAlign:"center",padding:6,fontSize:14}}>
+                        <strong>~Employment</strong>
+                    </div>
+                    <div className="col-xs-8" style={{textAlign:"center",padding:6,fontSize:14}}>
+                        {nf.format(estEmp)}
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-4" style={{textAlign:"center",padding:6,fontSize:16}}>
+                        <strong>Display:</strong>
+                    </div>
+                    <div className="col-xs-8">
+                        <div className="btn-group" role="group" >
+                          <button type="button" className={"btn btn-default " + cluster} onClick={this.setMode.bind(null,"cluster")}>Industry</button>
+                          <button type="button" className={"btn btn-default " + zips} onClick={this.setMode.bind(null,"zips")}>Geography</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    },
+
+    renderSunburst(){
+        let name = !this.state.options.naics.name || this.state.options.naics.name === "zbp" ? "All Industries" : this.state.options.naics.code+" - "+this.state.options.naics.name;
+
+        return (
+            <div className="col-md-12" style={{textAlign:"center"}}>
+                <h2>
+
+                {name}
+
+                </h2>
+                <svg id="sunburst" style={{width:"300px",height:"300px"}} />
+                {this.renderIndustryAnalysis()}
+            </div>
+        )
+    },
+
+    render:function(){
+
+        let loading = (
+            <div style={{position:"fixed",top:"50%",left:"50%"}}>
+             <Loading type="balls" color="#e3e3e3"  />
+            </div>
+        )
+
+        if(!this.state.loading){
+            loading = <span />
+        }
+
+        return (
+            <div className="container main">
+                <h1>Zip Business Patterns</h1>
+                <div className="row">
+                    <div className="col-md-12">
+                        <div id="nytg-tooltip">
+                            <div id="nytg-tooltipContainer">
+                                <div className="nytg-department"></div>
+                                <div className="nytg-rule"></div>
+                                <div className="nytg-name"></div>
+                                <div className="nytg-discretion"></div>
+                                <div className="nytg-valuesContainer">
+                                    <span className="nytg-value"></span>
+                                    <span className="nytg-change"></span>
+                                </div>
+                                <div className="nytg-chart"></div>
+                                <div className="nytg-tail"></div>
+                            </div>
+                        </div>
+
+                        {loading}
+                        <svg id="circles" style={{width:this.props.mapWidth,height:this.props.mapHeight}} >
+                            <g id="circle_group" />
+                            <g id="zip_group" />
+                        </svg>
+
+                        <div style={{position: "fixed","top": 100,"left":40,width:330}}>
+                            <div className="row">
+
+                                {this.renderControls()}
+
+                                {this.renderSunburst()}
+                            </div>
+                        </div>
+
+                        <div style={{position: "fixed","top": 100,"right":40,width:330}}>
+                            <div className="row">
+                                <svg id="circleLegend" style={{width:300,height:200}} />
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        );
+    }
+
 });
 
 export default Header;
